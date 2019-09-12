b5e360e9f392221a02e1c6897ec39ed4cebcd3a3
Madi <mhight@gmail.com>
2019-09-12 01:54:29 -0700

    avoiding assets folder

---
+++ Caltopia v2/Library/Collab/Download/Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : GameEvent
+{
+    public float delay = 30f;
+    public override void Run()
+    {
+        StartCoroutine(PlayMusicWithDelay());
+    }
+    
+    IEnumerator PlayMusicWithDelay()
+    {
+        if (!executed)
+        {
+            yield return new WaitForSeconds(delay);
+            executed = true;
+        }
+        StartCoroutine(PlayAudioClips());
+    }
+}
